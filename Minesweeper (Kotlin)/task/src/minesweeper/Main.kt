4008e42c52fc0a34734eb8d2264658be1aaef02f
denys <denamyte@gmail.com>
2023-04-03 02:43:58 +0300

    2023-04-03: Stage 1/5

--- Minesweeper (Kotlin)/task/src/minesweeper/Main.kt
+++ Minesweeper (Kotlin)/task/src/minesweeper/Main.kt
@@ -1,5 +1,20 @@
 package minesweeper
 
+const val MINE = "X"
+const val SAFE_CELL = "."
+const val FIELD_HEIGHT = 9
+const val FIELD_WIDTH = 9
+
 fun main() {
-    print("Hello, World!")
+    val field = List(FIELD_HEIGHT) { MutableList(FIELD_WIDTH) { SAFE_CELL } }
+    setMine(field, 1, 1)
+    printField(field)
+}
+
+fun setMine(field: List<MutableList<String>>, y: Int, x: Int) {
+    field[y][x] = MINE
+}
+
+fun printField(field: List<List<String>>) {
+    field.forEach { println(it.joinToString("")) }
 }
