986487ef6aee0c3e95e28a0bc6d7a3ffc26cc7f3
denys <denamyte@gmail.com>
2023-04-04 03:00:48 +0300

    2023-04-04: Stage 2/5

--- Minesweeper (Kotlin)/task/src/minesweeper/Main.kt
+++ Minesweeper (Kotlin)/task/src/minesweeper/Main.kt
@@ -1,18 +1,36 @@
 package minesweeper
 
+import kotlin.random.Random
+
 const val MINE = "X"
 const val SAFE_CELL = "."
 const val FIELD_HEIGHT = 9
 const val FIELD_WIDTH = 9
+const val MINES_PROMPT = "How many mines do you want on the field? "
 
 fun main() {
     val field = List(FIELD_HEIGHT) { MutableList(FIELD_WIDTH) { SAFE_CELL } }
-    setMine(field, 1, 1)
+    val mineCount = inputInt(MINES_PROMPT)
+    setRandomMines(field, mineCount)
     printField(field)
 }
 
-fun setMine(field: List<MutableList<String>>, y: Int, x: Int) {
-    field[y][x] = MINE
+fun inputInt(prompt: String): Int {
+    print(prompt)
+    return readln().toInt()
+}
+
+fun setRandomMines(field: List<MutableList<String>>, mineCount: Int) {
+    val random = Random.Default
+    var remain = mineCount
+    while (remain > 0) {
+        val row = random.nextInt(FIELD_HEIGHT)
+        val col = random.nextInt(FIELD_WIDTH)
+        if (field[row][col] == SAFE_CELL) {
+            remain--
+            field[row][col] = MINE
+        }
+    }
 }
 
 fun printField(field: List<List<String>>) {
